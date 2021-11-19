bfdd071adea4db08a0775b83ab81b8ab56f241fe
Audrow Nash <audrow@hey.com>
2021-11-19 13:01:44 -0800

    Support getting repo status
    Signed-off-by: Audrow Nash <audrow@hey.com>

--- update-maintainers/src/index.ts
+++ update-maintainers/src/index.ts
@@ -8,8 +8,14 @@ import {
   stringify as yamlStringify,
 } from "../deps.ts";
 
-import { getRepo, getRepoParts, listItems } from "./utils/index.ts";
-import { updateMaintainers } from "./maintainer-files/index.ts";
+import {
+  getPathsToFiles,
+  getRepo,
+  getRepoParts,
+  isPeopleListsTheSame,
+  listItems,
+} from "./utils/index.ts";
+import { getMaintainers, updateMaintainers } from "./maintainer-files/index.ts";
 import type { UpdateError } from "./maintainer-files/types.ts";
 import type {
   Config,
@@ -24,13 +30,13 @@ import {
   pushBranch,
 } from "./cli/index.ts";
 import {
-  getMaintainers,
+  getMaintainersFromIds,
   load as loadConfig,
   makeConfig,
   updateConfig,
 } from "./config/index.ts";
 
-async function main(config: Config) {
+async function updateRepoMaintainers(config: Config) {
   const options = config.options;
 
   if (!options.pullRequestTitle) {
@@ -61,7 +67,10 @@ async function main(config: Config) {
 
     let maintainers: Maintainer[] = [];
     try {
-      maintainers = getMaintainers(repo.maintainerIds, config.maintainers);
+      maintainers = getMaintainersFromIds(
+        repo.maintainerIds,
+        config.maintainers,
+      );
     } catch (error) {
       allErrors.push({ path: repo.path, error });
       continue;
@@ -122,6 +131,152 @@ async function main(config: Config) {
   }
 }
 
+type repoStatus = Pick<Repository, "branch" | "path" | "url"> & {
+  isUpToDate: boolean;
+};
+
+async function getMaintainerStatus(config: Config) {
+  const options = config.options;
+
+  const tempDir = "temp";
+  if (await exists(tempDir)) {
+    if (options.isOverwrite) {
+      await Deno.remove(tempDir, { recursive: true });
+    } else {
+      throw new Error("Temp directory already exists");
+    }
+  }
+
+  const repositoryStatuses: repoStatus[] = [];
+  const allErrors: UpdateError[] = [];
+  for (const repo of config.repositories) {
+    if (!repo.path) {
+      const { orgName, repoName } = getRepoParts(repo.url);
+      const dest = join(tempDir, orgName, repoName);
+      await clone(repo.url, dest);
+      repo.path = dest;
+      await checkoutBranch(repo.path, repo.branch, false);
+    }
+
+    let desiredMaintainers: Maintainer[] = [];
+    try {
+      desiredMaintainers = getMaintainersFromIds(
+        repo.maintainerIds,
+        config.maintainers,
+      );
+    } catch (error) {
+      allErrors.push({ path: repo.path, error });
+      continue;
+    }
+
+    const paths = await getPathsToFiles(repo.path, [
+      /package.xml$/,
+      /setup.py$/,
+    ]);
+    const updateErrors: UpdateError[] = [];
+    let isUpToDate = true;
+    for (const path of paths) {
+      try {
+        const fileText = await Deno.readTextFile(path);
+        const currentMaintainers = getMaintainers(path, fileText);
+        isUpToDate &&= isPeopleListsTheSame(
+          currentMaintainers,
+          desiredMaintainers,
+        );
+      } catch (error) {
+        updateErrors.push({ path, error });
+      }
+    }
+    repositoryStatuses.push({
+      ...repo,
+      isUpToDate,
+    });
+  }
+  if (allErrors.length > 0) {
+    console.error(
+      `\n${allErrors.length} errors occurred while getting maintainer status.`,
+    );
+    console.log("Errors:");
+    allErrors.forEach((error) => {
+      console.error(`- ${error.error.message}`);
+    });
+  }
+  const notUpToDateRepos = repositoryStatuses.filter((r) => !r.isUpToDate);
+  if (notUpToDateRepos.length > 0) {
+    console.error(
+      `\n${notUpToDateRepos.length} out of ${repositoryStatuses.length} repositories are not up to date.`,
+    );
+    console.log("Repositories:");
+    notUpToDateRepos.forEach((repo) => {
+      console.log(`- ${getRepo(repo.url)}`);
+    });
+  } else {
+    console.log(
+      `All ${repositoryStatuses.length} repositories are up to date.`,
+    );
+  }
+}
+
+async function createConfigFile(options: {
+  output: string;
+  maintainersFile?: string;
+  repositoriesFile?: string;
+  dryRun?: boolean;
+}) {
+  const defaultOptions: Options = {
+    isVerbose: false,
+    isDryRun: false,
+    isOverwrite: true,
+    pullRequestTitle: "Update maintainers",
+    workingBranch: "update-maintainers",
+  };
+  let maintainers: Maintainer[] = [];
+  if (options.maintainersFile) {
+    maintainers = (await yamlParse(
+      await Deno.readTextFile(options.maintainersFile),
+    ) as { maintainers: Maintainer[] }).maintainers;
+  } else {
+    maintainers = [
+      {
+        id: "audrow",
+        name: "Audrow Nash",
+        email: "audrow@openrobotics.org",
+      },
+      {
+        id: "notaudrow",
+        name: "Not Audrow",
+        email: "notaudrow@openrobotics.org",
+      },
+    ];
+  }
+  let repositories: Repository[] = [];
+  if (options.repositoriesFile) {
+    repositories = (await yamlParse(
+      await Deno.readTextFile(options.repositoriesFile),
+    ) as { repositories: Repository[] }).repositories;
+  } else {
+    repositories = [
+      {
+        url: "https://github.com/audrow/ros2cli",
+        branch: "master",
+        maintainerIds: ["audrow", "notaudrow"],
+      },
+    ];
+  }
+
+  const config = makeConfig({
+    options: defaultOptions,
+    maintainers,
+    repositories,
+  });
+  const configText = yamlStringify(config);
+  if (options.dryRun) {
+    console.log(configText);
+  } else {
+    await Deno.writeTextFile(options.output, configText);
+  }
+}
+
 const cli = cac("update-maintainers");
 
 cli.option("-v, --verbose", "verbose output");
@@ -152,10 +307,21 @@ cli
       workingBranch: options.workingBranch,
     };
     const config = updateConfig(readConfig, { options: configOptions });
-    await main(config);
+    await updateRepoMaintainers(config);
   });
 
-cli.command("make-config", "Make a config file")
+cli
+  .command("status", "Get the status of the maintainers")
+  .option("-c, --config <path>", "Path to config file", {
+    default: "config.yaml",
+  })
+  .action(async (options) => {
+    const config = await loadConfig(options.config);
+    await getMaintainerStatus(config);
+  });
+
+cli
+  .command("make-config", "Make a config file")
   .option("-o, --output <path>", "Path to output file", {
     default: "config.yaml",
   })
@@ -163,58 +329,7 @@ cli.command("make-config", "Make a config file")
   .option("--repositories-file <path>", "Path to repositories YAML file")
   .option("--dry-run", "Print the text, don't save the file")
   .action(async (options) => {
-    const defaultOptions: Options = {
-      isVerbose: false,
-      isDryRun: false,
-      isOverwrite: true,
-      pullRequestTitle: "Update maintainers",
-      workingBranch: "update-maintainers",
-    };
-    let maintainers: Maintainer[] = [];
-    if (options.maintainersFile) {
-      maintainers = (await yamlParse(
-        await Deno.readTextFile(options.maintainersFile),
-      ) as { maintainers: Maintainer[] }).maintainers;
-    } else {
-      maintainers = [
-        {
-          id: "audrow",
-          name: "Audrow Nash",
-          email: "audrow@openrobotics.org",
-        },
-        {
-          id: "notaudrow",
-          name: "Not Audrow",
-          email: "notaudrow@openrobotics.org",
-        },
-      ];
-    }
-    let repositories: Repository[] = [];
-    if (options.repositoriesFile) {
-      repositories = (await yamlParse(
-        await Deno.readTextFile(options.repositoriesFile),
-      ) as { repositories: Repository[] }).repositories;
-    } else {
-      repositories = [
-        {
-          url: "https://github.com/audrow/ros2cli",
-          branch: "master",
-          maintainerIds: ["audrow", "notaudrow"],
-        },
-      ];
-    }
-
-    const config = makeConfig({
-      options: defaultOptions,
-      maintainers,
-      repositories,
-    });
-    const configText = yamlStringify(config);
-    if (options.dryRun) {
-      console.log(configText);
-    } else {
-      await Deno.writeTextFile(options.output, configText);
-    }
+    await createConfigFile(options);
   });
 
 cli.help();
